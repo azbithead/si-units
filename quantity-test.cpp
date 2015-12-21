1de3e289dc9d9297e8ddd772fc1663ccdc4b9e28
Steve Hales <steve.hales@garmin.com>
2015-12-21 11:02:47 -0700

    Added unit tests for si::is_quantity and si::gcd.

--- quantity-test.cpp
+++ quantity-test.cpp
@@ -2,6 +2,14 @@
 
 int main( int aArgCount, char* aArgs[] )
 {
-    // si::is_units
-//    static_assert( !si::is_units< int >, "" );
+    // si::is_quantity
+    static_assert( !si::is_quantity< int >, "" );
+    static_assert( si::is_quantity< si::quantity<si::meters,int> >, "" );
+    static_assert( si::is_quantity< const si::quantity<si::meters,int> >, "" );
+    static_assert( si::is_quantity< volatile si::quantity<si::meters,int> >, "" );
+    static_assert( si::is_quantity< const volatile si::quantity<si::meters,int> >, "" );
+
+    // si::gcd
+    static_assert( si::gcd<12,9> == 3, "" );
+    static_assert( si::gcd<7,5> == 1, "" );
 }
